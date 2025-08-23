d0985324529ff42462668f4ca6c245de425fa553
Diyorbek Abdumutalibov <diordev9@gmail.com>
2025-08-24 00:18:07 +0500

    Disigne Structe Folder SDK

---
+++ src/prelude.rs
@@ -0,0 +1,6 @@
+//! SDK foydalanuvchilari uchun qulay re-exports
+//!
+//! Odatda siz `use katm_sdk::prelude::*;` qilib import qilasiz
+//! va asosiy turlarni to‘g‘ridan-to‘g‘ri ishlatishingiz mumkin.
+
+pub use crate::error::{KatmError, KatmResult};
